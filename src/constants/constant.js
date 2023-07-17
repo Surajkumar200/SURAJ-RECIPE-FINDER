89d2a61c902d4b4c69811777f2856e39f84e972d
Suraj Kumar Shaw <surajshaw7778@gmail.com>
2023-07-18 02:29:09 +0530

    first commit

---
+++ src/constants/constant.js
@@ -0,0 +1,4 @@
+export const logo = 'https://th.bing.com/th/id/OIP.kUkJDUUeYjoHBeyZ70eILgHaFE?pid=ImgDet&w=219&h=139.7872340425532&c=7';
+
+
+export const API_URL = 'https://forkify-api.herokuapp.com/api';
